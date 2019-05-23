03384d4a2f6239080a27b215c83c9ebea17eeaea
Xang Thao <xangthao24@gmail.com>
2019-05-23 16:18:23 -0700

    Able to generate random cars and collision detection

--- src/Game.js
+++ src/Game.js
@@ -3,16 +3,26 @@ import purpleCar from "./assets/images/purpleCar.png"
 import fireTruck from "./assets/images/fireTruck.png"
 import yellowCar from "./assets/images/yellowCar.png"
 import yellowBus from "./assets/images/yellowBus.png"
-import redCar from "./assets/images/red_car.png"
 import grass from "./assets/images/grass.jpg"
 import road from "./assets/images/road.jpg"
 import river from "./assets/images/river.gif"
-const gameState = {}
+import whiteVan from "./assets/images/whiteVan.png"
+import spaceCar from "./assets/images/spaceCar.png"
+import greySport from "./assets/images/greySport.png"
+import policeCar from "./assets/images/policeCar.png"
+import redSport from "./assets/images/redSport.png"
+import redCar from "./assets/images/redCar.png"
+import greenTruck from "./assets/images/greenTruck.png"
+
+const gameState = {
+  lives: 3
+}
 
 class Game extends Phaser.Scene {
   constructor() {
     super({key: "Game"})
   }
+
   preload() {
     this.load.image("grass", grass)
     this.load.image("road", road)
@@ -20,10 +30,15 @@ class Game extends Phaser.Scene {
     this.load.image("fireTruck", fireTruck)
     this.load.image("yellowCar", yellowCar)
     this.load.image("yellowBus", yellowBus)
-    this.load.image("redCar", redCar)
     this.load.image("river", river)
+    this.load.image("whiteVan", whiteVan)
+    this.load.image("spaceCar", spaceCar)
+    this.load.image("greySport", greySport)
+    this.load.image("policeCar", policeCar)
+    this.load.image("redCar", redCar)
+    this.load.image("redSport", redSport)
+    this.load.image("greenTruck", greenTruck)
   }
-
   create() {
     const rowHeight = 42.9;
     const halfRowHeight = rowHeight/2;
@@ -53,85 +68,13 @@ class Game extends Phaser.Scene {
     score.displayHeight =  carLogHeight
     score.displayWidth=800
 
-    // let road1 = this.add.rectangle(400, 12 * rowHeight + halfRowHeight, 800, rowHeight, '#0fffff')
-    // grass.displayHeight =  carLogHeight
-    // grass.displayWidth=800
 
-    // let road1 = this.add.rectangle(400, 11 * rowHeight + halfRowHeight, 800, rowHeight, '#0fffff')
-    // grass.displayHeight =  carLogHeight
-    // grass.displayWidth=800
 
-    // let road1 = this.add.rectangle(400, 10 * rowHeight + halfRowHeight, 800, rowHeight, '#0fffff')
-    // grass.displayHeight =  carLogHeight
-    // grass.displayWidth=800
-
-    // let road1 = this.add.rectangle(400, 9 * rowHeight + halfRowHeight, 800, rowHeight, '#0fffff')
-    // grass.displayHeight =  carLogHeight
-    // grass.displayWidth=800
-
-    // let road1 = this.add.rectangle(400, 8 * rowHeight + halfRowHeight, 800, rowHeight, '#0fffff')
-    // grass.displayHeight =  carLogHeight
-    // grass.displayWidth=800
-
-    gameState.redCar = this.physics.add.sprite(600,  5 * rowHeight + halfRowHeight, "purpleCar")
+    gameState.redCar = this.physics.add.sprite(400,  13 * rowHeight + halfRowHeight, "purpleCar")
     gameState.redCar.displayWidth=800*.1; 
-    // gameState.redCar.displayHeight=600*.1; 
-    // height of the canvas * (13 rows/(2/height))
-    // height * (13/(2/height))
     gameState.redCar.displayHeight= carLogHeight;
     gameState.redCar.setCollideWorldBounds(true)
 
-    gameState.redCar1 = this.physics.add.sprite(600, halfRowHeight, "fireTruck")
-    gameState.redCar1.displayWidth=800*.2; 
-    gameState.redCar1.displayHeight= carLogHeight; 
-
-    gameState.redCar2 = this.physics.add.sprite(600, rowHeight + halfRowHeight, "yellowBus")
-    gameState.redCar2.displayWidth=800*.2; 
-    gameState.redCar2.displayHeight= carLogHeight; 
-
-    gameState.redCar3 = this.physics.add.sprite(600, 2 * rowHeight + halfRowHeight, "yellowCar")
-    gameState.redCar3.displayWidth=800*.1; 
-    gameState.redCar3.displayHeight= carLogHeight; 
-
-    gameState.redCar4 = this.physics.add.sprite(600, 3 * rowHeight + halfRowHeight, "redCar")
-    gameState.redCar4.displayWidth=800*.1; 
-    gameState.redCar4.displayHeight= carLogHeight; 
-
-    gameState.redCar5 = this.physics.add.sprite(600,  4 * rowHeight + halfRowHeight, "purpleCar")
-    gameState.redCar5.displayWidth=800*.1; 
-    gameState.redCar5.displayHeight= carLogHeight; 
-
-    gameState.redCar6 = this.physics.add.sprite(600, 6 * rowHeight + halfRowHeight, "yellowCar")
-    gameState.redCar6.displayWidth=800*.1; 
-    gameState.redCar6.displayHeight= carLogHeight; 
-
-    gameState.redCar7 = this.physics.add.sprite(600, 7 * rowHeight + halfRowHeight, "yellowCar")
-    gameState.redCar7.displayWidth=800*.1; 
-    gameState.redCar7.displayHeight= carLogHeight; 
-
-    gameState.redCar8 = this.physics.add.sprite(600, 8 * rowHeight + halfRowHeight, "yellowCar")
-    gameState.redCar8.displayWidth=800*.1; 
-    gameState.redCar8.displayHeight= carLogHeight; 
-
-    gameState.redCar9 = this.physics.add.sprite(600, 9 * rowHeight + halfRowHeight, "yellowCar")
-    gameState.redCar9.displayWidth=800*.1; 
-    gameState.redCar9.displayHeight= carLogHeight; 
-
-    gameState.redCar10 = this.physics.add.sprite(600, 10 * rowHeight + halfRowHeight, "yellowCar")
-    gameState.redCar10.displayWidth=800*.1; 
-    gameState.redCar10.displayHeight= carLogHeight; 
-
-    gameState.redCar11 = this.physics.add.sprite(600, 11 * rowHeight + halfRowHeight, "yellowCar")
-    gameState.redCar11.displayWidth=800*.1; 
-    gameState.redCar11.displayHeight= carLogHeight; 
-
-    gameState.redCar12 = this.physics.add.sprite(600, 12 * rowHeight + halfRowHeight, "yellowCar")
-    gameState.redCar12.displayWidth=800*.1; 
-    gameState.redCar12.displayHeight= carLogHeight; 
-
-    gameState.redCar13 = this.physics.add.sprite(600, 13 * rowHeight + halfRowHeight, "yellowCar")
-    gameState.redCar13.displayWidth=800*.1; 
-    gameState.redCar13.displayHeight= carLogHeight; 
 
     let text = this.add.text(50, 50, "testing")
     text.setInteractive()
@@ -140,80 +83,214 @@ class Game extends Phaser.Scene {
 			this.scene.start('Landing')
     })
 
+    const vehicles = this.physics.add.group();
+  
+    function genItem1 () {
+      let cars = ["whiteVan", "policeCar", "purpleCar"]
+      let randomCar = Math.floor(Math.random() * 3)
+      let roadY1 = 8 * rowHeight + halfRowHeight
+      let vehicle = vehicles.create(10, roadY1, cars[randomCar])
+      vehicle.displayWidth=800*.1
+      vehicle.displayHeight = carLogHeight
+      vehicle.setVelocityX(75)
+      
+    }
+
+    const bugGenLoop = this.time.addEvent({
+      delay: 4000,
+      callback: genItem1,
+      callbackScope: this,
+      loop: true
+    });
+
+    this.physics.add.collider(gameState.redCar, vehicles, function(redCar) {
+      redCar.destroy();
+      // delete gameState.redCar
+      gameState.lives -= 1
+      gameState.livesLeft.setText(`Lives ${gameState.lives}`)
+      
+      gameState.redCar = this.physics.add.sprite(400,  13 * rowHeight + halfRowHeight, "purpleCar")
+        gameState.redCar.displayWidth=800*.1; 
+        gameState.redCar.displayHeight= carLogHeight;
+        gameState.redCar.setCollideWorldBounds(true)
+      if(gameState.lives === 0) {
+        this.scene.stop('Game')
+        this.scene.start('Landing')
+        gameState.lives = 3
+      }
+    }.bind(this));
+
+    const vehicles2 = this.physics.add.group();
+
+    function genItem2 () {
+      let cars = ["redCar", "spaceCar", "whiteVan"]
+      let randomCar = Math.floor(Math.random() * 3)
+      let roadY1 = 9 * rowHeight + halfRowHeight
+      let vehicle = vehicles2.create(800, roadY1, cars[randomCar])
+      vehicle.displayWidth=800*.1
+      vehicle.displayHeight = carLogHeight
+      vehicle.setVelocityX(-95)
+      
+    }
+
+    const bugGenLoop2 = this.time.addEvent({
+      delay: 6000,
+      callback: genItem2,
+      callbackScope: this,
+      loop: true
+    });
+
+    this.physics.add.collider(gameState.redCar, vehicles2, function(redCar) {
+      redCar.destroy();
+      // delete gameState.redCar
+      gameState.lives -= 1
+      if(gameState.lives > 0) {
+        gameState.redCar = this.physics.add.sprite(400,  13 * rowHeight + halfRowHeight, "purpleCar")
+        gameState.redCar.displayWidth=800*.1; 
+        gameState.redCar.displayHeight= carLogHeight;
+        gameState.redCar.setCollideWorldBounds(true)
+      } else {
+        this.scene.stop('Game')
+        this.scene.start('Landing')
+        gameState.lives = 3
+      }
+    }.bind(this));
+
+    const vehicles3 = this.physics.add.group();
+
+    function genItem3 () {
+
+      let roadY1 = 10 * rowHeight + halfRowHeight
+      let vehicle = vehicles3.create(10, roadY1, 'purpleCar')
+      vehicle.displayWidth=800*.1
+      vehicle.displayHeight = carLogHeight
+      vehicle.setVelocityX(70)
+      
+    }
+
+    const bugGenLoop3 = this.time.addEvent({
+      delay: 5000,
+      callback: genItem3,
+      callbackScope: this,
+      loop: true
+    });
+
+    this.physics.add.collider(gameState.redCar, vehicles3, function(redCar) {
+      redCar.destroy();
+      // delete gameState.redCar
+      gameState.lives -= 1
+      gameState.redCar = this.physics.add.sprite(400,  13 * rowHeight + halfRowHeight, "purpleCar")
+        gameState.redCar.displayWidth=800*.1; 
+        gameState.redCar.displayHeight= carLogHeight;
+        gameState.redCar.setCollideWorldBounds(true)
+      if(gameState.lives === 0) {
+        this.scene.stop('Game')
+        this.scene.start('Landing')
+        gameState.lives = 3
+      }
+    }.bind(this));
+
+    const vehicles4 = this.physics.add.group();
+
+    function genItem4 () {
+      let cars = ["yellowCar", "spaceCar", "greySport"]
+      let randomCar = Math.floor(Math.random() * 3)
+
+      let roadY1 = 11 * rowHeight + halfRowHeight
+      let vehicle = vehicles4.create(800, roadY1, cars[randomCar])
+      vehicle.displayWidth=800*.1
+      vehicle.displayHeight = carLogHeight
+      vehicle.setVelocityX(-85)
+      
+    }
+
+    const bugGenLoop4 = this.time.addEvent({
+      delay: 3000,
+      callback: genItem4,
+      callbackScope: this,
+      loop: true
+    });
+
+    this.physics.add.collider(gameState.redCar, vehicles4, function(redCar) {
+      redCar.destroy();
+      // delete gameState.redCar
+      gameState.lives -= 1
+      if(gameState.lives > 0) {
+        gameState.redCar = this.physics.add.sprite(400,  13 * rowHeight + halfRowHeight, "purpleCar")
+        gameState.redCar.displayWidth=800*.1; 
+        gameState.redCar.displayHeight= carLogHeight;
+        gameState.redCar.setCollideWorldBounds(true)
+      } else {
+        this.scene.stop('Game')
+        this.scene.start('Landing')
+        gameState.lives = 3
+      }
+    }.bind(this));
+
+    const vehicles5 = this.physics.add.group();
+
+    function genItem5 () {
+      let cars = ["fireTruck", "yellowBus", "whiteVan"]
+      let randomCar = Math.floor(Math.random() * 3)
+
+      let roadY1 = 12 * rowHeight + halfRowHeight
+      let vehicle = vehicles5.create(10, roadY1, cars[randomCar])
+      vehicle.displayWidth=800*.2
+      vehicle.displayHeight = carLogHeight
+      vehicle.setVelocityX(55)
+      
+    }
+
+    const bugGenLoop5 = this.time.addEvent({
+      delay: 7500,
+      callback: genItem5,
+      callbackScope: this,
+      loop: true
+    });
+
+    this.physics.add.collider(gameState.redCar, vehicles5, function(redCar) {
+      redCar.destroy();
+      // delete gameState.redCar
+      gameState.lives -= 1
+      if(gameState.lives > 0) {
+        gameState.redCar = this.physics.add.sprite(400,  13 * rowHeight + halfRowHeight, "purpleCar")
+        gameState.redCar.displayWidth=800*.1; 
+        gameState.redCar.displayHeight= carLogHeight;
+        gameState.redCar.setCollideWorldBounds(true)
+      } else {
+        this.scene.stop('Game')
+        this.scene.start('Landing')
+        gameState.lives = 3
+      }
+    }.bind(this));
+
     this.input.keyboard.on('keyup_LEFT', function (event) {
-      gameState.redCar.x -= 18.4
+      gameState.redCar.x -= rowHeight
     })
     this.input.keyboard.on('keyup_RIGHT', function (event) {
-      gameState.redCar.x += 18.4
+      gameState.redCar.x += rowHeight
     })
 
     this.input.keyboard.on('keyup_DOWN', function (event) {
-      gameState.redCar.y += 18.4
+      gameState.redCar.y += rowHeight
     })
 
     this.input.keyboard.on('keyup_UP', function (event) {
-      gameState.redCar.y -= 18.4
+      gameState.redCar.y -= rowHeight
     })
+
+    gameState.livesLeft = this.add.text(400, 10, `Lives ${gameState.lives}`)
 
   }
 
   update() {
     
 
-    if(gameState.redCar && gameState.redCar.x <= 100) {
-      gameState.redCar.setVelocityX(100)
-      // gameState.redCar.destroy()
+    // if(gameState.redCar && gameState.redCar.x <= 100) {
+    //   gameState.redCar.setVelocityX(100)
+    //   // gameState.redCar.destroy()
       // delete gameState.redCar
-    }
-    if(gameState.redCar.x >= 500) {
-      gameState.redCar.setVelocityX(-100)
-    }
-    
-    // gameState.redCar1.setVelocityX(-100);
-    if(gameState.redCar1 && gameState.redCar1.x <= 100) {
-      gameState.redCar1.setVelocityX(100)
-      // gameState.redCar1.destroy()
-      // delete gameState.redCar1
-    }
-    if(gameState.redCar1.x >= 500) {
-      gameState.redCar1.setVelocityX(-100)
-    }
-
-    if(gameState.redCar2 && gameState.redCar2.x <= 100) {
-      gameState.redCar2.setVelocityX(100)
-      // gameState.redCar2.destroy()
-      // delete gameState.redCar2
-    }
-    if(gameState.redCar2.x >= 500) {
-      gameState.redCar2.setVelocityX(-100)
-    }
-  
-    if(gameState.redCar3 && gameState.redCar3.x <= 100) {
-      gameState.redCar3.setVelocityX(100)
-      // gameState.redCar3.destroy()
-      // delete gameState.redCar3
-    }
-    if(gameState.redCar3.x >= 500) {
-      gameState.redCar3.setVelocityX(-100)
-    }
-   
-    if(gameState.redCar4 && gameState.redCar4.x <= 100) {
-      gameState.redCar4.setVelocityX(100)
-      // gameState.redCar4.destroy()
-      // delete gameState.redCar4
-    }
-    if(gameState.redCar4.x >= 500) {
-      gameState.redCar4.setVelocityX(-100)
-    }
-   
-    if(gameState.redCar5 && gameState.redCar5.x <= 100) {
-      gameState.redCar5.setVelocityX(100)
-      // gameState.redCar5.destroy()
-      // delete gameState.redCar5
-    }
-    if(gameState.redCar5.x >= 500) {
-      gameState.redCar5.setVelocityX(-100)
-    }
+    // }
     
   }
 }
